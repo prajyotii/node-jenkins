d5270a82b8440b9f1f2a8b31112773ea196ad64d
prajyotii <141770405+prajyotii@users.noreply.github.com>
2023-08-17 11:17:33 +0530

    Update prepare-tests.sh

--- prepare-tests.sh
+++ prepare-tests.sh
@@ -1,5 +1,27 @@
-pwd
-ls
-mkdir "test-data"
+
+echo "##########################"
+echo 'The following "npm" command runs your Node.js application'
 echo "##########################"
 ls
+set -x
+
+npm start &
+sleep 1
+echo $! > .pidfile
+ls
+node -v
+ls
+sudo zip tar.zip .
+ls
+sudo zip -r tar.zip .
+ls
+cp tar.zip ~/test
+sudo unzip tar.zip
+npm install
+npm start
+
+ls
+set +x
+
+echo 'Now you can'
+echo 'Visit http://localhost:3000 to see your Node.js application'
